d5d507cbb0956d405b5e9a59e43b6f6f681d84cf
Jorge Lozano <Jorge.Lozano@solera.com>
2022-04-07 10:55:32 +0200

    added basic controller and service

---
+++ AsyncComms.ApiGateway/Controller/MessageController.cs
@@ -0,0 +1,42 @@
+﻿namespace AsyncComms.ApiGateway.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {  
+        // GET: api/<MessageController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _messages;
+        }
+
+        // GET api/<MessageController>/5
+        [HttpGet("{index}")]
+        public string Get(int index)
+        {
+            return _messages[index];
+        }
+
+        // POST api/<MessageController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _messages.Add(value);
+        }
+
+        // PUT api/<MessageController>/5
+        [HttpPut("{index}")]
+        public void Put(int index, [FromBody] string value)
+        {
+            _messages[index] = value;
+        }
+
+        // DELETE api/<MessageController>/5
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            _messages.RemoveAt(index);
+        }
+    }
+}
